9e43fb3afff429546ec6f409e6dcc59b3d9514d4
Tania Perrella <taniasperrella@gmail.com>
2020-10-22 22:12:17 -0300

    Adicionando Imagens e alteração de Layout

--- app/src/main/java/com/wcc/lancedados/MainActivity.kt
+++ app/src/main/java/com/wcc/lancedados/MainActivity.kt
@@ -11,13 +11,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val botaoLancarDados = findViewById<Button>(R.id.lanceDadosBotao)
-        val valorDados1 = findViewById<TextView>(R.id.numero1)
-        val valorDados2 = findViewById<TextView>(R.id.numero2)
-
 
         botaoLancarDados.setOnClickListener {
-            valorDados1.text = lancarDados().toString()
-            valorDados2.text = lancarDados().toString()
         }
     }
 

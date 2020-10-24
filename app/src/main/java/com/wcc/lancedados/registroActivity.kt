0da64ab663612d4e09a21b5519c3593e6d119387
Tania Perrella <taniasperrella@gmail.com>
2020-10-24 11:42:51 -0300

    Adicionando Layout Tela de Registro

--- app/src/main/java/com/wcc/lancedados/registroActivity.kt
+++ app/src/main/java/com/wcc/lancedados/registroActivity.kt
@@ -3,9 +3,9 @@ package com.wcc.lancedados
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class CadastroActivity : AppCompatActivity() {
+class registroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cadastro)
+        setContentView(R.layout.activity_registro)
     }
 }

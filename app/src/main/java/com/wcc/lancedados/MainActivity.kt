ab51bcdeb639af84f319e0c66dbb765d79865800
Tania Perrella <taniasperrella@gmail.com>
2020-10-24 10:54:24 -0300

    Adicionando Imagens e alteração de Layout

--- app/src/main/java/com/wcc/lancedados/MainActivity.kt
+++ app/src/main/java/com/wcc/lancedados/MainActivity.kt
@@ -3,6 +3,7 @@ package com.wcc.lancedados
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -11,12 +12,33 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val botaoLancarDados = findViewById<Button>(R.id.lanceDadosBotao)
+        val dado1 = findViewById<ImageView>(R.id.dado1)
+        val dado2 = findViewById<ImageView>(R.id.dado2)
+
+
 
         botaoLancarDados.setOnClickListener {
+            dado1.setImageResource(lancarDados())
+            dado2.setImageResource(lancarDados())
         }
     }
 
     fun lancarDados(): Int{
+        return recuperaFaceDado(gerarNumero())
+    }
+
+    fun recuperaFaceDado(numero: Int): Int {
+        return when(numero){
+            1-> R.drawable.dice_1
+            2-> R.drawable.dice_2
+            3-> R.drawable.dice_3
+            4-> R.drawable.dice_4
+            5-> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+    }
+
+    fun gerarNumero(): Int{
         return (1..6).random()
     }
 }
